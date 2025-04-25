6189cf9670a0546397ec4d7e7995ff9de92f3ef7
RolandoGuZz <150936260+RolandoGuZz@users.noreply.github.com>
2025-04-25 08:55:20 -0600

    Update carousel_view.dart

--- lib/screens/carousel_view.dart
+++ lib/screens/carousel_view.dart
@@ -4,9 +4,16 @@ class CarouselViewX extends StatelessWidget {
   const CarouselViewX({super.key});
   @override
   Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+   
     return Scaffold(
       appBar: AppBar(title: Text('Home Screen')),
-      body: Container(),
+      body: Column(
+       children: [
+        Container(height: size.height * .3, width: double.infinity, color: Colors.red, child: Text('Texto en el container')),
+       ],
+      ),
     );
   }
 }
